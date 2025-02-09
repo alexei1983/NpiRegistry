fd57918d0108b39243622272e81447455f82689a
Alex Jennings <alex.jennings@persante.com>
2025-02-09 15:37:36 -0700

    Bump EnhancedWebRequest to 1.1.1

--- NpiRegistry/NpiRegistryRequest.cs
+++ NpiRegistry/NpiRegistryRequest.cs
@@ -30,29 +30,8 @@ namespace Llc.GoodConsulting.Interfaces.NpiRegistry
         /// <returns></returns>
         protected async virtual Task<TResponse?> Execute<TResponse>(Dictionary<string, string>? parameters = null) where TResponse : class, new()
         {
-            var urlParameters = GetQueryString(parameters) ?? string.Empty;
-            var entity = await request.MakeRequest<TResponse>(urlParameters, "GET");
-            if (entity == null || !entity.IsSuccess)
-                throw new Exception("NPI registry returned a non-successful response.");
-            return entity?.Entity;
-        }
-
-        /// <summary>
-        /// Builds a url-encoded query string for the specified parameter(s).
-        /// </summary>
-        /// <param name="parameters">Parameters to url-encode into a query string.</param>
-        /// <returns><see cref="string"/></returns>
-        protected virtual string? GetQueryString(Dictionary<string, string>? parameters)
-        {
-            if (parameters?.Count > 0)
-            {
-                var queryString = "?";
-                foreach (var parameter in parameters)
-                    queryString += $"{parameter.Key}={Uri.EscapeDataString(parameter.Value)}&";
-
-                return queryString.TrimEnd('&');
-            }
-            return null;
+            var response = await request.Get(parameters ?? []);
+            return await response.ExpectSuccess().AsJsonEntityAsync<TResponse>();
         }
     }
 }
